5c3748fc8f4a10283bea80f746394b28faffb659
Randy <piggy01@students.cs.ubc.ca>
2026-02-11 22:13:40 -0800

    Added tests for UserInfo

---
+++ src/test/model/TestUserInfo.java
@@ -0,0 +1,119 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestUserInfo {
+
+    private String testPhrases = "data/testPhrases.txt";
+
+    private UserInfo userInfo;
+
+    private Round testRound1;
+    private Round testRound2;
+    private Round testRound3;
+    private final int seed1 = 1;
+    private final int seed2 = 2;
+    private final int seed3 = 3;
+
+    
+
+    @BeforeEach
+    void setUp() {
+        testRound1 = new Round(testPhrases, new Random(seed1));
+        testRound2 = new Round(testPhrases, new Random(seed2));
+        testRound3 = new Round(testPhrases, new Random(seed3));
+        userInfo = new UserInfo();
+    }
+
+    @Test
+    void testAddSavedPhrase() {
+        ArrayList<String> testList = new ArrayList<>();
+
+        userInfo.addSavedPhrase(testRound1.getActualText());
+        testList.add(testRound1.getActualText());
+        assertEquals(1, userInfo.getNumberOfSavedPhrases());
+        assertEquals(testList, userInfo.getSavedPhrases());
+    }
+
+    @Test
+    void testAddMultipleSavedPhrase() {
+        ArrayList<String> testList = new ArrayList<>();
+
+        userInfo.addSavedPhrase(testRound1.getActualText());
+        testList.add(testRound1.getActualText());
+        assertEquals(1, userInfo.getNumberOfSavedPhrases());
+        assertEquals(testList, userInfo.getSavedPhrases());
+        // !!! seeded differently?
+        userInfo.addSavedPhrase(testRound2.getActualText());
+        testList.add(testRound2.getActualText());
+        assertEquals(2, userInfo.getNumberOfSavedPhrases());
+        assertEquals(testList, userInfo.getNumberOfSavedPhrases());
+    }
+
+    @Test
+    void testAddSavedPhraseDuplicate() {
+        ArrayList<String> testList = new ArrayList<>();
+
+        userInfo.addSavedPhrase(testRound1.getActualText());
+        testList.add(testRound1.getActualText());
+        assertEquals(1, userInfo.getNumberOfSavedPhrases());
+        assertEquals(testList, userInfo.getSavedPhrases());
+
+        // !!! test try catch?
+        userInfo.addSavedPhrase(testRound1.getActualText());
+        assertEquals(1, userInfo.getNumberOfSavedPhrases());
+        assertEquals(testList, userInfo.getSavedPhrases());
+    }
+
+    @Test
+    void testAddPastRound() {
+        ArrayList<Round> testList = new ArrayList<>();
+
+        userInfo.addPastRound(testRound1);
+        testList.add(testRound1);
+        assertEquals(1, userInfo.getNumberOfPastRounds());
+        assertEquals(testList, userInfo.getPastRounds());
+    }
+
+    @Test
+    void testAddMultiplePastRound() {
+        ArrayList<Round> testList = new ArrayList<>();
+
+        userInfo.addPastRound(testRound1);
+        testList.add(testRound1);
+        assertEquals(1, userInfo.getNumberOfPastRounds());
+        assertEquals(testList, userInfo.getPastRounds());
+
+        userInfo.addPastRound(testRound2);
+        testList.add(testRound2);
+        assertEquals(2, userInfo.getNumberOfPastRounds());
+        assertEquals(testList, userInfo.getPastRounds());
+    }
+
+    // rounds are rarely duplicate, and if they somehow are, I don't see
+    // why there can't be duplicates.
+    @Test
+    void testAddPastRoundDuplicate() {
+        ArrayList<Round> testList = new ArrayList<>();
+
+        userInfo.addPastRound(testRound1);
+        testList.add(testRound1);
+        assertEquals(1, userInfo.getNumberOfPastRounds());
+        assertEquals(testList, userInfo.getPastRounds());
+
+        userInfo.addPastRound(testRound1);
+        testList.add(testRound1);
+        assertEquals(2, userInfo.getNumberOfPastRounds());
+        assertEquals(testList, userInfo.getPastRounds());
+    }
+
+}
