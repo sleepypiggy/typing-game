d836130d7f30d6bcbb30abf0532d1e49c4a0ea7a
Randy <piggy01@students.cs.ubc.ca>
2026-02-11 22:32:49 -0800

    Added implementation for UserInfo class.

---
+++ src/main/exceptions/DuplicatePhraseException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class DuplicatePhraseException extends Exception {
+
+}
--- src/main/model/UserInfo.java
+++ src/main/model/UserInfo.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import exceptions.DuplicatePhraseException;
 
 // The UserInfo class represents all the information of the user,
 // including the past rounds that the user has played (where each round
@@ -13,19 +14,45 @@ public class UserInfo {
     ArrayList<String> savedPhrases;
     
     public UserInfo() {
-
+        this.pastRounds = new ArrayList<>();
+        this.savedPhrases = new ArrayList<>();
     }
 
     // MODIFIES: this
-    // EFFECTS: adds phrase to the list of saved phrases, savedPhrases.
+    // EFFECTS: adds phrase to the list of saved phrases, savedPhrases. If phrase is 
+    //          already in savedPhrases, don't add it and notify user.
     public void addSavedPhrase(String phrase) {
-
+        try {
+            if (!this.savedPhrases.contains(phrase)) {
+                this.savedPhrases.add(phrase);
+            } else {
+                throw new DuplicatePhraseException();
+            }
+        } catch (DuplicatePhraseException e) {
+            System.out.println("You already saved this phrase. ");
+        }
     }
 
     // MODIFIES: this
     // EFFECTS: adds round to the list of past rounds played, pastRounds.
     public void addPastRound(Round round) {
+        this.pastRounds.add(round);
+    }
 
+    public ArrayList<String> getSavedPhrases() {
+        return savedPhrases;
+    }
+
+    public int getNumberOfSavedPhrases() {
+        return savedPhrases.size();
+    }
+
+    public ArrayList<Round> getPastRounds() {
+        return pastRounds;
+    }
+
+    public int getNumberOfPastRounds() {
+        return pastRounds.size();
     }
 
 }
