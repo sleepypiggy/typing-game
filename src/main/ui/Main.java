1105aa2155418ee923ec527b052e8927d49a3901
Randy <piggy01@students.cs.ubc.ca>
2026-02-15 14:39:14 -0800

    Added Main class including specification and game instantiation.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,12 @@
 package ui;
 
+// The main class where the program instantiates the required objects to run, which
+// in this case, is the TypingGame() object.
+
 public class Main {
+ 
+    // EFFECTS: instantiates a TypingGame() object to start the program.
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to my project!");
+        new TypingGame();
     }
 }
