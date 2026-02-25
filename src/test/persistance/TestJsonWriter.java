ffaec2cce3ae911e87dd1b92e3a6039eed257dd6
Randy <piggy01@students.cs.ubc.ca>
2026-02-25 15:53:25 -0800

    Added tests for JsonWriter class.

---
+++ src/test/persistance/TestJsonWriter.java
@@ -0,0 +1,67 @@
+package persistance;
+
+import model.Round;
+import model.UserInfo;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestJsonWriter extends TestJson {
+
+    @Test
+    void testWriterInvalidFile() {
+        try {
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testWriterEmptyUserInfo() {
+        try {
+            UserInfo userInfo = new UserInfo();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyUserInfo.json");
+            writer.open();
+            writer.write(userInfo);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyUserInfo.json");
+            userInfo = reader.read();
+            assertEquals(0, userInfo.getNumberOfSavedPhrases());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterGeneralWorkroom() {
+        try {
+            UserInfo userInfo = new UserInfo();
+            userInfo.addSavedPhrase(new StringBuilder("test phrase 1"));
+            userInfo.addSavedPhrase(new StringBuilder("test phrase 2"));
+            JsonWriter writer = new JsonWriter("./data/testWriterUserInfo.json");
+            writer.open();
+            writer.write(userInfo);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterUserInfo.json");
+            userInfo = reader.read();
+            List<String> phrases = userInfo.getSavedPhrases();
+            assertEquals(2, phrases.size());
+            checkPhrase("test phrase 1", phrases.get(0));
+            checkPhrase("test phrase 2", phrases.get(1));
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+}
