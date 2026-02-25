d4d2011fbea77daf37a98790e77914e6a1415efe
Randy <piggy01@students.cs.ubc.ca>
2026-02-25 15:56:31 -0800

    Implemented JsonWriter class.

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,47 @@
+package persistence;
+
+import model.UserInfo;
+import org.json.JSONObject;
+
+import java.io.*;
+
+// Represents a writer that writes JSON representation of the user's info to file.
+public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs a writer to write to the destination file. 
+    public JsonWriter(String destination) {
+        this.destination = destination;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+        writer = new PrintWriter(new File(destination));
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of user info to file
+    public void write(UserInfo userInfo) {
+        JSONObject json = userInfo.toJson();
+        saveToFile(json.toString(TAB));
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        writer.close();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    public void saveToFile(String json) {
+        writer.print(json);
+    }
+
+
+
+}
