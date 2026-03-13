488d9882efde4d96d760ce41d33db85045b34cda
Randy <piggy01@students.cs.ubc.ca>
2026-03-13 16:27:03 -0700

    Added specification for GameUI class.

---
+++ src/main/ui/GameUI.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.*;
+
+// This class displays all the UI elements for anything that is on the main game screen.
+
+public class GameUI extends JPanel {
+
+    public GameUI() {
+
+    }
+
+}
