f1417975065bbdd3ef16674631ab42f54d0cab9b
Randy <piggy01@students.cs.ubc.ca>
2026-03-13 16:27:12 -0700

    Added specification for EndUI class.

---
+++ src/main/ui/EndUI.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.*;
+
+// This class displays all the UI elements for anything that is on the end screen.
+
+public class EndUI extends JPanel {
+
+    public EndUI() {
+
+    }
+
+}
