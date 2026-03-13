9b4dadb0c75ea76fabb7637642b7939c6b960f77
Randy <piggy01@students.cs.ubc.ca>
2026-03-13 16:26:29 -0700

    Added specification for GameWindow class.

---
+++ src/main/ui/GameWindow.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+// This class is responsible for creating the window that displays the user interface. It also handles
+// the logic that switches between different "screens" like the menu screen, the main game screen, and
+// the end screen.
+
+public class GameWindow extends JFrame {
+
+    public GameWindow() {
+        
+    }
+    
+}
