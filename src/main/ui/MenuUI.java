1932a15baa00622d681a99b2da748ddb687a620f
Randy <piggy01@students.cs.ubc.ca>
2026-03-13 16:26:51 -0700

    Added specification for MenuUI class.

---
+++ src/main/ui/MenuUI.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// This class displays all the UI elements for anything that is on the menu screen.
+
+public class MenuUI extends JPanel {
+
+    public MenuUI() {
+        
+    }
+
+}
