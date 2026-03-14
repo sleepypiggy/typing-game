d370669894fad6c0062223a74b43ed111a2384f1
Randy <piggy01@students.cs.ubc.ca>
2026-03-13 19:53:58 -0700

    Implemented JFrame and UI switching in GameWindow class.

--- src/main/ui/GameWindow.java
+++ src/main/ui/GameWindow.java
@@ -8,9 +8,40 @@ import java.awt.*;
 // the end screen.
 
 public class GameWindow extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    private MenuUI menuUI;
+    private GameUI gameUI;
+    private EndUI endUI;
 
     public GameWindow() {
-        
+        menuUI = new MenuUI(this);
+        gameUI = new GameUI();
+        endUI = new EndUI();
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        mainPanel.add(menuUI, "menu");
+        mainPanel.add(gameUI, "game");
+        mainPanel.add(endUI, "end");
+
+        setExtendedState(Frame.MAXIMIZED_BOTH);
+        setVisible(true);
+        //setResizable(false);
+
+        add(mainPanel);
     }
-    
+
+    // EFFECTS: displays the GameUI class.
+    public void switchToGameUI() {
+        cardLayout.show(mainPanel, "game");
+    }
+
+    // EFFECTS: displays the EndUI class.
+    public void switchToGameOverUI() {
+        cardLayout.show(mainPanel, "end");
+    }
+
 }
