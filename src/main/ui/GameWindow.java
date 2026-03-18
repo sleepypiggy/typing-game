d0758e1b3bcb5c6ee49bdc43452324ab7dfce326
Randy <piggy01@students.cs.ubc.ca>
2026-03-17 17:10:17 -0700

    Specified GameWindow class.

--- src/main/ui/GameWindow.java
+++ src/main/ui/GameWindow.java
@@ -1,47 +1,38 @@
 package ui;
 
+import model.Round;
+import model.UserInfo;
+
 import javax.swing.*;
+
+import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
+
 import java.awt.*;
 
 // This class is responsible for creating the window that displays the user interface. It also handles
 // the logic that switches between different "screens" like the menu screen, the main game screen, and
 // the end screen.
 
+@ExcludeFromJacocoGeneratedReport
 public class GameWindow extends JFrame {
-    private CardLayout cardLayout;
-    private JPanel mainPanel;
 
-    private MenuUI menuUI;
-    private GameUI gameUI;
-    private EndUI endUI;
-
-    public GameWindow() {
-        menuUI = new MenuUI(this);
-        gameUI = new GameUI();
-        endUI = new EndUI();
-
-        cardLayout = new CardLayout();
-        mainPanel = new JPanel(cardLayout);
-
-        mainPanel.add(menuUI, "menu");
-        mainPanel.add(gameUI, "game");
-        mainPanel.add(endUI, "end");
-
-        setExtendedState(Frame.MAXIMIZED_BOTH);
-        setVisible(true);
-        //setResizable(false);
-
-        add(mainPanel);
+    public GameWindow(TypingGame typingGame, Round currentRound, UserInfo userInfo) {
+        
     }
 
     // EFFECTS: displays the GameUI class.
     public void switchToGameUI() {
-        cardLayout.show(mainPanel, "game");
+        
     }
 
     // EFFECTS: displays the EndUI class.
     public void switchToGameOverUI() {
-        cardLayout.show(mainPanel, "end");
+        
     }
 
+    // MODIFIES: this
+    // EFFECTS: updates the current round used by this.
+    public void updateCurrentRound(Round currentRound) {
+        
+    }
 }
