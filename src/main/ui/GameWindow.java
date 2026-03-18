c35b5d0184304b6bebbe579a178cd9b5c7c7c8cb
Randy <piggy01@students.cs.ubc.ca>
2026-03-17 17:10:51 -0700

    Implemented GameWindow class.

--- src/main/ui/GameWindow.java
+++ src/main/ui/GameWindow.java
@@ -15,24 +15,50 @@ import java.awt.*;
 
 @ExcludeFromJacocoGeneratedReport
 public class GameWindow extends JFrame {
+    private TypingGame typingGame;
+    private Round currentRound;
+    private UserInfo userInfo;
+
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    private MenuUI menuUI;
+    private GameUI gameUI;
+    private EndUI endUI;
 
     public GameWindow(TypingGame typingGame, Round currentRound, UserInfo userInfo) {
-        
+        //menuUI = new MenuUI(this);
+        //gameUI = new GameUI(currentRound);
+        this.typingGame = typingGame;
+        this.currentRound = currentRound;
+        this.userInfo = userInfo;
+        endUI = new EndUI(typingGame, currentRound, userInfo);
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        //mainPanel.add(menuUI, "menu");
+        //mainPanel.add(gameUI, "game");
+        mainPanel.add(endUI, "end");
+
+        setExtendedState(Frame.MAXIMIZED_BOTH);
+        setVisible(true);
+        //setResizable(false);
+
+        add(mainPanel);
     }
 
     // EFFECTS: displays the GameUI class.
     public void switchToGameUI() {
-        
+        cardLayout.show(mainPanel, "game");
     }
 
     // EFFECTS: displays the EndUI class.
     public void switchToGameOverUI() {
-        
+        cardLayout.show(mainPanel, "end");
     }
 
-    // MODIFIES: this
-    // EFFECTS: updates the current round used by this.
     public void updateCurrentRound(Round currentRound) {
-        
+        this.currentRound = currentRound;
     }
 }
