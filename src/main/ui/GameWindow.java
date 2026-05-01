02ab30bf18c4f4ceab244ea7e303c62d2f3c0099
Randy <randydu2007@gmail.com>
2026-04-30 20:32:36 -0700

    Updated GameWindow class to accomodate new UI classes.

--- src/main/ui/GameWindow.java
+++ src/main/ui/GameWindow.java
@@ -18,27 +18,31 @@ public class GameWindow extends JFrame {
     private TypingGame typingGame;
     private Round currentRound;
     private UserInfo userInfo;
+    private ButtonSpriteSheet buttonSpriteSheet;
 
     private CardLayout cardLayout;
     private JPanel mainPanel;
 
     private EndUI endUI;
+    private MenuUI menuUI;
+    private GameUI gameUI;
 
     // EFFECTS: instantiates the associated information so that the GUI can interact with and display information,
     //          and creates the window for the GUI to appear in, as well as the GUI itself.
     public GameWindow(TypingGame typingGame, Round currentRound, UserInfo userInfo) {
-        //menuUI = new MenuUI(this);
-        //gameUI = new GameUI(currentRound);
         this.typingGame = typingGame;
         this.currentRound = currentRound;
         this.userInfo = userInfo;
-        endUI = new EndUI(typingGame, currentRound, userInfo);
+        this.buttonSpriteSheet = new ButtonSpriteSheet();
+        endUI = new EndUI(typingGame, currentRound, userInfo, this.buttonSpriteSheet, this);
+        menuUI = new MenuUI(typingGame, currentRound, userInfo, this.buttonSpriteSheet, this);
+        gameUI = new GameUI(typingGame, currentRound, userInfo);
 
         cardLayout = new CardLayout();
         mainPanel = new JPanel(cardLayout);
 
-        //mainPanel.add(menuUI, "menu");
-        //mainPanel.add(gameUI, "game");
+        mainPanel.add(menuUI, "menu");
+        mainPanel.add(gameUI, "game");
         mainPanel.add(endUI, "end");
 
         setExtendedState(Frame.MAXIMIZED_BOTH);
@@ -48,15 +52,15 @@ public class GameWindow extends JFrame {
         add(mainPanel);
     }
 
-    // // EFFECTS: displays the GameUI class.
-    // public void switchToGameUI() {
-    //     cardLayout.show(mainPanel, "game");
-    // }
+    // EFFECTS: displays the GameUI class.
+    public void switchToGameUI() {
+        cardLayout.show(mainPanel, "game");
+    }
 
-    // // EFFECTS: displays the EndUI class.
-    // public void switchToGameOverUI() {
-    //     cardLayout.show(mainPanel, "end");
-    // }
+    // EFFECTS: displays the EndUI class.
+    public void switchToGameOverUI() {
+        cardLayout.show(mainPanel, "end");
+    }
 
     // MODIFIES: this
     // EFFECTS: updates the current round of this to currentRound.
